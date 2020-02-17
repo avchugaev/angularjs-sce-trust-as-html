4110747ae396d7802e45f6d74fef17d6c1a55303
avchugaev <achugaev93@gmail.com>
2020-02-17 22:34:33 +0200

    Add trusted styles demo

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ angular.module("editorApp", [])
   <h1>trustAsHtml Example</h1>
   <p>You should see a "Secure code" message in the console log.</p>
   <p>You should not see a "Danger!!!" message in the console log because of AngularJS SCE protection which prevents JavaScript execution from embedded HTML.</p>
+  <p>Embedded content may contain styles which will be applied!</p>
   <script>
     console.warn('Danger!!!')
   </script>
+  <style>
+    body {
+      color: white;
+      background: green;
+    }
+  </style>
   `);
   console.log('Secure code')
 });
